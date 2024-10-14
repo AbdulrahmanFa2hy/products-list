fb7bc10239f71009153420a223f0c025cb3b8780
AbdulrahmanFa2hy <abdelrhman.fa2hy@gmail.com>
2024-10-14 10:08:01 +0300

    update

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+  const { items, del, handleQuantity } = props;
+  let length = items.length;
 
-export default Items
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <p className="quantity-content">
+            <span
+              onClick={() => handleQuantity({ id: item.id, type: "decrement" })}
+            >
+              -
+            </span>
+            <span className="count">{item.quantity}</span>
+            <span
+              onClick={() => handleQuantity({ id: item.id, type: "increment" })}
+            >
+              +
+            </span>
+          </p>
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+        </div>
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Edit</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
+
+export default Items;
